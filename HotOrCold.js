acc7178c7d0f76cd9b2bc6e5d60f3bea618e4d53
giteup <dharman@4harman.com>
2013-11-09 14:00:16 -0600

    2013_11.09 - Update / no progress!
    I can not get jQuery to run!  I'm using the same code as our 37 Signals
    project and yet the text won't toggle!  Frustrating.  I'm going to
    re-read my notes and quit coding for today.

--- HotOrCold.js
+++ HotOrCold.js
@@ -1,29 +1,6 @@
+$(document).ready (function () {
+    $(".bigBox").click(function () {
+    $(".a, .b").toggleClass ("hide");
+});
 
 
-/*
-
-function getRandomInt(min,max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-alert(getRandomInt(10,1000));
-
-function game() {
-var answer = getRandomInt;
-var guess = document.userInput.value;
-
-while (guess != answer) {
-    guess = prompt("Your guess?", " ");
-    if (guess == answer) {
-        alert("You win!");
-        break;
-    }
-    else if (guess < answer || guess > answer) {
-        prompt("Try again!");
-    }
-    else if (guess == "NaN") {
-        prompt("Numbers only!");
-    }
-}
-}
-
-*/
