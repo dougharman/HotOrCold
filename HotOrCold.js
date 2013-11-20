be7e50a5662682fa2c0f7c54ccb004ce761ebfd0
giteup <dharman@4harman.com>
2013-11-20 07:33:34 -0600

    2013_11_20 - Update - yesterday's work

--- HotOrCold.js
+++ HotOrCold.js
@@ -5,19 +5,10 @@ function getRandomInt(min, max) {
 var target = getRandomInt(1, 100);
 console.log(target);
 
-//  John, code I found the showBanner code by Googling for "something" that would sequentially
-//  show my messages.  Original code is in "testCode" file. 
+//  John, code I found the original showBanner code by Googling for "something" that would sequentially
+//  show my messages; however, I couldn't get it to work.  (Original code is in "testCode" file.)
+//  so I found the following which does - sorta, i.e., it still happens too fast
 
-/*function showBanner(guess) {
-    var lis = $(".banner ul li").hide();  
-    if (guess == target) { 
-        var i = 0;  
-        (function displayText() {  
-            lis.eq(i++).fadeIn(200, displayText);  
-        })()  
-   }  
-}
-*/
 
 var eT = 0;     
 function showBanner(guess) {
@@ -29,8 +20,18 @@ function showBanner(guess) {
     }
 }
 
+var p = [];                                // "p" as in previous guess
+function showGuesses(guess) {
+    p.unshift(guess); 
+}
+
+var count = p.length;
+alert(count);
+
+
 function checkAnswer(guess) {
     console.log(guess);
+    alert(count);
     if (guess == target) {
         $(".messageBox p:first").append("<h2>You Win!</h2>");
         $(".messageBox p:nth-child(2)").empty();
@@ -39,23 +40,31 @@ function checkAnswer(guess) {
         $(".gc2, .gurl").removeClass("hide");
         $(".gurl").attr("align","center");
     
-// John, plugin code isn't working...
-/*        $(".messageBox").blinker({
-            interval: 500,
-            endcolor: '#fff',
-            colors: [
-            '#111',
-            '#222',
-            '#333'
-            ]
-            });    */
+        // John, plugin code for "modern blink"
+      
+        $(".messageBox p:first").modernBlink({
+            duration: 1000,
+            iterationCount: "5",
+            auto: true
+        })
+
     } else if (guess < 1 || guess > 100) {
         $(".messageBox p:nth-child(2)").append("<h2>Please enter a number between 1 and 100!</h2>");
     } else if (isNaN(guess)) {
         $(".messageBox p:nth-child(2)").append("<h2>Please enter a number!</h2>");
-    } else {
+    } else if (count == 1) { 
         $(".messageBox p:nth-child(2)").append("<h2>Try again!</h2>");
-    }
+    } else if (guess < p[-1])&&(p[-1] < target) {
+        $(".messageBox p:nth-child(2)").append("<h2>You're Getting Colder</h2>"); 
+    } else if (target < p[-1])&&(p[-1] < guess) {
+        $(".messageBox p:nth-child(2)").append("<h2>You're Getting Colder</h2>");
+    } else if (p[-1] < guess)&&(guess < target) {
+        $(".messageBox p:nth-child(2)").append("<h2>You're Getting Hotter<h2>");
+    } else if (target < guess)&&(guess < p[-1]) {
+        $(".messageBox p:nth-child(2)").append("<h2>You're Getting Hotter<h2>");
+    } else {
+        $(".messageBox p:nth-child(2)").append("<h2>Try again!</h2>")
+    }       
 }        
 
 $(document).ready(function () {
@@ -65,8 +74,9 @@ $(document).ready(function () {
         if (event.which == 13) {
             checkAnswer($("#inputBox").val());
             showBanner($("#inputBox").val());
+            showGuesses($("#inputBox").val());
             var toAdd = $("#inputBox").val();
-            $(".displayOfGuess").append("<p>" + toAdd + "</p>");
+            $(".displayOfGuess").append("<p>" + p + "</p>");
             event.preventDefault();
         }
 
@@ -83,11 +93,8 @@ $(document).ready(function () {
     $("#submitButton").click(function () {
         checkAnswer($("#inputBox").val());
         showBanner($("#inputBox").val());
-        var toAdd = $("#inputBox").val();
-        $(".displayOfGuess").append("<p>" + toAdd + "</p>");
-        setTimeout(function () {
-        var toAdd = $("#inputBox").val();
-            }, 100);
+        showGuesses($("#inputBox").val());
+        $(".displayOfGuess").append("<p>" + p + "</p>");
         event.preventDefault();
     });
 
@@ -120,13 +127,5 @@ So, I'm doing the following - and forgetting about clearing the input box for th
         $(".messageBox").hide();
         $(".gc2, .gurl").hide();
     }); 
-
-
-
-
-
-
-
-
 
 });
